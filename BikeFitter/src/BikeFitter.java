0f8bb5ae8251afa47940f71a022761ed318bc181
NiallYoung1 <Niall_is@live.co.uk>
2013-10-04 17:50:36 +0100

    BikeFitter

---
+++ BikeFitter/src/BikeFitter.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Niallyoung
+ */
+public class BikeFitter {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        FittingForm form = new FittingForm();
+        form.setVisible(true);
+        
+    }
+    
+    public static void quit(){
+        //Quits and Closes the Application
+    }
+}
